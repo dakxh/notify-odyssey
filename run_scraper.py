ec21ae33afda1a525e90bb2d9495c9a383f11dff
Dakxh Dakxh <89646853+dakxh@users.noreply.github.com>
2026-07-18 20:42:48 +0530

    Update run_scraper.py

--- run_scraper.py
+++ run_scraper.py
@@ -8,7 +8,7 @@ import subprocess
 from datetime import datetime
 
 # --- CONFIGURATION ---
-DATES = ["20260718", "20260719"]
+DATES = ["20260719"]
 VENUE_CODE = "PRHN"
 EVENT_CODE = "ET00452034"
 STATE_FILE = "state.json"
